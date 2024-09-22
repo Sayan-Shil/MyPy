20a8c5ff191c22d5e79762ae642ba36077ed035a
Sayan Shil <sayansil2000@gmail.com>
2024-09-22 20:36:08 +0530

    Pandas Added

---
+++ Pandas/0_Intro.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+
+#Series: a one-dimensional labeled array holding data of any type
+# such as integers, strings, Python objects etc.
+
---
+++ Pandas/1_Pandas_Series.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+mySeries = [1,2,3,4,5,6]
+pandaSeries = pd.Series(mySeries)
+print(pandaSeries)
+
